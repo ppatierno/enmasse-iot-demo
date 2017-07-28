25a87b2085ae4721b91c26c23a7147045fcf0ca9
Paolo Patierno <ppatierno@live.com>
2017-07-28 15:14:56 +0200

    Moved scripts for Spark + Kubernetes demo in a related directory Deleted EnMasse 0.11.2 release directory

--- scripts/spark_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Deploying Apache Spark ..."
-kubectl create -f ../spark-kubernetes/spark-master.yaml -n enmasse-spark
-kubectl create -f ../spark-kubernetes/spark-master-service.yaml -n enmasse-spark
-
-kubectl create -f ../spark-kubernetes/spark-worker.yaml -n enmasse-spark
-echo "... Apache Spark deployed"
